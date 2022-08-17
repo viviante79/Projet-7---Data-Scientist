bce76994555f5d9db68af3154361231ca8e53ec9
viviante79 <104625942+viviante79@users.noreply.github.com>
2022-08-17 21:02:14 +0200

    Update ors_vivian_6_api_072022.py

--- ors_vivian_6_api_072022.py
+++ ors_vivian_6_api_072022.py
@@ -15,4 +15,5 @@ df = data[data["SK_ID_CURR"] == id_filter]
 # Affichage de la prédiction :
 query = df.drop(columns=["TARGET","SK_ID_CURR"])
 score = list(model.predict_proba(query))
-st.json({"Id" : data["SK_ID_CURR"], "score" : score[0][0]})
+st.json({"Id" : df["SK_ID_CURR"], "score" : score[0][0]})
+
