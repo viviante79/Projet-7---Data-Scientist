7b9f548b71ba0fdcd56f0d0f4df60fab16f2e8de
viviante79 <104625942+viviante79@users.noreply.github.com>
2022-08-17 20:29:52 +0200

    Update ors_vivian_5_api_072022.py

--- ors_vivian_5_api_072022.py
+++ ors_vivian_5_api_072022.py
@@ -6,14 +6,18 @@ import joblib
 # Import des fichiers, du modèle et du scaler :
 data = pd.read_csv("dataset/data_api.csv", index_col=[0])
 model = joblib.load("dataset/LGBM_model")
-scaler = joblib.load("dataset/Scaler")
+
+# Calcul de la prédiction :
+prediction = int(model.predict(df.drop(columns=["TARGET","SK_ID_CURR"])))
+proba = model.predict_proba(df.drop(columns=["TARGET","SK_ID_CURR"]))
+proba = proba[0][0],1
+
+df = pd.DataFrame(data["SK_ID_CURR"], pediction, proba)
+st.dataframe(df)
 
 # Filtre :
 id_filter = st.selectbox("Entrez identifiant client", pd.unique(data["SK_ID_CURR"]))
 df = data[data["SK_ID_CURR"] == id_filter]
 
-# Affichage de la prédiction :
-prediction = int(model.predict(df.drop(columns=["TARGET","SK_ID_CURR"])))
-proba = model.predict_proba(df.drop(columns=["TARGET","SK_ID_CURR"]))
-proba = round(proba[0][0]*100,1)
+
 st.write(prediction, proba)
